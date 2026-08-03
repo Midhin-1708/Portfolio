3d360210f6d9494e6a2a40fd33c53f27a3c4b9ab
Midhin <midhin.mugundan@gmail.com>
2026-08-03 17:13:38 +0530

    Updated the Import details in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { validateContact } from "../utils/validation";
+import validateContact from "../utils/validation";
 
 function Contact() {
   const [formData, setFormData] = useState({
